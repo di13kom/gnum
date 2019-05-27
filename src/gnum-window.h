8ecfa1699f303a64e8f50d2d8bb0c2c7b1debfd1
di13kom <di13kom@gmail.com>
2019-05-27 22:33:31 +0300

    Created directory structure

---
+++ src/gnum-window.h
@@ -0,0 +1,29 @@
+/* gnum-window.h
+ *
+ * Copyright 2019 kom
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define GNUM_TYPE_WINDOW (gnum_window_get_type())
+
+G_DECLARE_FINAL_TYPE (GnumWindow, gnum_window, GNUM, WINDOW, GtkApplicationWindow)
+
+G_END_DECLS
